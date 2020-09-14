d0c0e4753229baaae939886a6bbe249fdbd24bcc
Jakov <klaric9jakov@gmail.com>
2020-09-14 20:20:18 +0200

    Finished comparison function, and protein name output.

--- ProteinSynthesisAid/ProteinSynthesisAid/Program.cs
+++ ProteinSynthesisAid/ProteinSynthesisAid/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
 
 namespace ProteinSynthesisAid
 {
@@ -31,58 +34,89 @@ namespace ProteinSynthesisAid
 
             var krt31 = new List<string>()
             {
-               met, phe, pro, tyr, asn, cys, asp, gln, glu, leu, ser, hsi, val, stop
+               "krt31", met, phe, pro, tyr, asn, cys, asp, gln, glu, leu, ser, hsi, val, stop
             };
             var tuba8 = new List<string>()
             {
-                met, ile, lys, phe, thr, ala, cys, gly, trp, arg, pro, tyr, ser, asp, gln, stop
+               "tuba8", met, ile, lys, phe, thr, ala, cys, gly, trp, arg, pro, tyr, ser, asp, gln, stop
             };
             var gloverin = new List<string>()
             {
-                met, gln, ser, tyr, leu, ala, hsi, arg, asn, trp, cys, val, pro, gln, stop
+               "gloverin", met, gln, ser, tyr, leu, ala, hsi, arg, asn, trp, cys, val, pro, gln, stop
             };
             var myosinI = new List<string>()
             {
-                met, hsi, tyr, leu,  ser, trp, lys, phe, thr, glu, cys, stop
+               "myosinI", met, hsi, tyr, leu,  ser, trp, lys, phe, thr, glu, cys, stop
             };
             var coro1A = new List<string>()
             {
-                met, asn, glu, gly, asp, pro, gln, cys, lys, phe, trp, ala, tyr, stop
+               "coro1A", met, asn, glu, gly, asp, pro, gln, cys, lys, phe, trp, ala, tyr, stop
             };
             var cadherin = new List<string>()
             {
-                met, lys, ala, glu, arg, trp, ile, val, asp, lys, pro, trp, phe, stop
+               "cadherin", met, lys, ala, glu, arg, trp, ile, val, asp, lys, pro, trp, phe, stop
             };
             var ependymin = new List<string>()
             {
-                met, tyr, trp, asp, ala, arg, asn, phe, pro, lys, cys, tyr, stop
+               "ependymin", met, tyr, trp, asp, ala, arg, asn, phe, pro, lys, cys, tyr, stop
             };
             var scramblase = new List<string>()
             {
-                met, hsi, lys, trp, tyr, thr, pro, leu, cys, asn, hsi, ser, asp, stop
+               "scramblase", met, hsi, lys, trp, tyr, thr, pro, leu, cys, asn, hsi, ser, asp, stop
             };
             var neurophysin = new List<string>()
             {
-                met, cys, hsi, pro, phe, tyr, ala, arg, trp, stop
+               "neurophysin", met, cys, hsi, pro, phe, tyr, ala, arg, trp, stop
             };
             var foxg1 = new List<string>()
             {
-                met, val, tyr, trp, hsi, asn, arg, ala, pro, thr, lys, cys, stop
+               "foxg1", met, val, tyr, trp, hsi, asn, arg, ala, pro, thr, lys, cys, stop
             };
             var srrt = new List<string>()
             {
-                met, hsi, trp, tyr, lys, glu, gln, cys, val, gly, thr, hsi, glu, asp, stop
+               "srrt", met, hsi, trp, tyr, lys, glu, gln, cys, val, gly, thr, hsi, glu, asp, stop
             };
             var ferritin = new List<string>()
             {
-                met, glu, tyr, val, hsi, cys, lys, trp, ala, gly, asn, asp, lys, gln, stop
+               "ferritin", met, glu, tyr, val, hsi, cys, lys, trp, ala, gly, asn, asp, lys, gln, stop
             };
             var groel = new List<string>()
             {
-                met, val, hsi, lys, cys, glu, gly, lys, leu, hsi, asp, ala, asn, stop
+               "groel", met, val, hsi, lys, cys, glu, gly, lys, leu, hsi, asp, ala, asn, stop
             };
 
+            var proteinList = new List<List<string>>()
+            {
+                krt31, tuba8, gloverin, myosinI, coro1A, cadherin, ependymin, scramblase, neurophysin, foxg1, srrt, ferritin, groel
+            };
 
+            //foreach (var aminoAcid in foxg1)
+            //{
+            //    Console.Write(aminoAcid);
+            //} 
+            //this is used for getting a valid sequence for debugging
+
+            Console.WriteLine();
+            Console.WriteLine("Please, enter an amino-acid sequence in codone triplets.");
+            var userInput = Console.ReadLine();
+
+            Console.WriteLine("The entered sequence corresponds with " + isProtein(userInput, proteinList));
+
+        }
+
+        static string isProtein(string userInput, List<List<string>> proteinList)
+        {
+            var proteinReturn = "no proteins.";
+
+            for (int i = 0; i < proteinList.Count - 1; i++)
+            {
+                if (userInput.Contains(string.Join("", proteinList[i].Skip(1))))
+                {
+                    proteinReturn = proteinList[i][0];
+                }
+            }
+
+            return proteinReturn;
         }
     }
 }
