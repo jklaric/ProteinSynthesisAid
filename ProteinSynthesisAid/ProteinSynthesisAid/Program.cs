01b00efa5d894932b7f042a5416faa9cc83325ad
Jakov <klaric9jakov@gmail.com>
2020-10-04 14:30:44 +0200

    Changed names to key, value pairs for easier use.

--- ProteinSynthesisAid/ProteinSynthesisAid/Program.cs
+++ ProteinSynthesisAid/ProteinSynthesisAid/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -36,56 +37,71 @@ namespace ProteinSynthesisAid
 
             var krt31 = new List<string>()
             {
-               "krt31", met, phe, pro, tyr, asn, cys, asp, gln, glu, leu, ser, hsi, val, stop
+               met, phe, pro, tyr, asn, cys, asp, gln, glu, leu, ser, hsi, val, stop
             };
             var tuba8 = new List<string>()
             {
-               "tuba8", met, ile, lys, phe, thr, ala, cys, gly, trp, arg, pro, tyr, ser, asp, gln, stop
+               met, ile, lys, phe, thr, ala, cys, gly, trp, arg, pro, tyr, ser, asp, gln, stop
             };
             var gloverin = new List<string>()
             {
-               "gloverin", met, gln, ser, tyr, leu, ala, hsi, arg, asn, trp, cys, val, pro, gln, stop
+               met, gln, ser, tyr, leu, ala, hsi, arg, asn, trp, cys, val, pro, gln, stop
             };
             var myosinI = new List<string>()
             {
-               "myosinI", met, hsi, tyr, leu,  ser, trp, lys, phe, thr, glu, cys, stop
+               met, hsi, tyr, leu,  ser, trp, lys, phe, thr, glu, cys, stop
             };
             var coro1A = new List<string>()
             {
-               "coro1A", met, asn, glu, gly, asp, pro, gln, cys, lys, phe, trp, ala, tyr, stop
+               met, asn, glu, gly, asp, pro, gln, cys, lys, phe, trp, ala, tyr, stop
             };
             var cadherin = new List<string>()
             {
-               "cadherin", met, lys, ala, glu, arg, trp, ile, val, asp, lys, pro, trp, phe, stop
+               met, lys, ala, glu, arg, trp, ile, val, asp, lys, pro, trp, phe, stop
             };
             var ependymin = new List<string>()
             {
-               "ependymin", met, tyr, trp, asp, ala, arg, asn, phe, pro, lys, cys, tyr, stop
+               met, tyr, trp, asp, ala, arg, asn, phe, pro, lys, cys, tyr, stop
             };
             var scramblase = new List<string>()
             {
-               "scramblase", met, hsi, lys, trp, tyr, thr, pro, leu, cys, asn, hsi, ser, asp, stop
+               met, hsi, lys, trp, tyr, thr, pro, leu, cys, asn, hsi, ser, asp, stop
             };
             var neurophysin = new List<string>()
             {
-               "neurophysin", met, cys, hsi, pro, phe, tyr, ala, arg, trp, stop
+               met, cys, hsi, pro, phe, tyr, ala, arg, trp, stop
             };
             var foxg1 = new List<string>()
             {
-               "foxg1", met, val, tyr, trp, hsi, asn, arg, ala, pro, thr, lys, cys, stop
+               met, val, tyr, trp, hsi, asn, arg, ala, pro, thr, lys, cys, stop
             };
             var srrt = new List<string>()
             {
-               "srrt", met, hsi, trp, tyr, lys, glu, gln, cys, val, gly, thr, hsi, glu, asp, stop
+               met, hsi, trp, tyr, lys, glu, gln, cys, val, gly, thr, hsi, glu, asp, stop
             };
             var ferritin = new List<string>()
             {
-               "ferritin", met, glu, tyr, val, hsi, cys, lys, trp, ala, gly, asn, asp, lys, gln, stop
+               met, glu, tyr, val, hsi, cys, lys, trp, ala, gly, asn, asp, lys, gln, stop
             };
             var groel = new List<string>()
             {
-               "groel", met, val, hsi, lys, cys, glu, gly, lys, leu, hsi, asp, ala, asn, stop
+               met, val, hsi, lys, cys, glu, gly, lys, leu, hsi, asp, ala, asn, stop
             };
+
+            var proteinNames = new Dictionary<string, List<string>>();
+            proteinNames.Add("krt31", krt31);
+            proteinNames.Add("tuba8", tuba8);
+            proteinNames.Add("gloverin", gloverin);
+            proteinNames.Add("myosinI", myosinI);
+            proteinNames.Add("coro1A", coro1A);
+            proteinNames.Add("cadherin", cadherin);
+            proteinNames.Add("ependymin", ependymin);
+            proteinNames.Add("scramblase", scramblase);
+            proteinNames.Add("neurophysin",neurophysin);
+            proteinNames.Add("foxg1", foxg1);
+            proteinNames.Add("srrt", srrt);
+            proteinNames.Add("ferritin", ferritin);
+            proteinNames.Add("groel", groel);
 
             var proteinList = new List<List<string>>()
             {
@@ -96,9 +112,9 @@ namespace ProteinSynthesisAid
             Console.WriteLine("Please, enter the name of a protein, or a codon sequence.");
             var userInput = Console.ReadLine();
 
-            var codon = FindCodon(userInput, proteinList);
+            var codon = FindCodon(userInput, proteinList, proteinNames);
 
-            if (FindCodon(userInput, proteinList) != "error")
+            if (FindCodon(userInput, proteinList, proteinNames) != "error")
             {
                 Console.WriteLine("The following is the sequence of protein " + userInput + ": " + codon);
                 Console.WriteLine("The anticodon of the protein sequence is " + FindAnticodon(codon));
@@ -107,35 +123,32 @@ namespace ProteinSynthesisAid
             {
                 codon = userInput;
 
-                Console.WriteLine("The sequence you entered corresponds with the protein : " + IsProtein(userInput, proteinList));
+                Console.WriteLine("The sequence you entered corresponds with the protein : " + IsProtein(userInput, proteinList, proteinNames));
                 Console.WriteLine("The anticodon of that protein is: " + FindAnticodon(codon));
             }
         }
 
-        static string FindCodon(string userInput, List<List<string>> proteinList)
+        static string FindCodon(string userInput, List<List<string>> proteinList, Dictionary<string, List<string>> proteinNames)
         {
             var proteinReturn = "error";
 
-            for (int i = 0; i < proteinList.Count - 1; i++)
-            {
-                if (userInput == proteinList[i][0])
+                if (proteinNames.ContainsKey(userInput))
                 {
-                    proteinReturn = string.Join("", proteinList[i].Skip(1));
+                    proteinReturn = string.Join("", proteinNames[userInput]);
                 }
-            }
 
             return proteinReturn;
         }
 
-        static string IsProtein(string userInput, List<List<string>> proteinList)
+        static string IsProtein(string userInput, List<List<string>> proteinList, Dictionary<string, List<string>> proteinNames)
         {
             var proteinReturn = "no proteins.";
 
-            for (int i = 0; i < proteinList.Count - 1; i++)
+            foreach (var value in proteinNames)
             {
-                if (userInput.Contains(string.Join("", proteinList[i].Skip(1))))
+                if (string.Join("", value.Value) == userInput)
                 {
-                    proteinReturn = proteinList[i][0];
+                    proteinReturn = value.Key;
                 }
             }
 
